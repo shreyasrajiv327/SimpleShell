307f37d528387b244c5c8de3375ddcfbcdf681e2
shreyasrajiv327 <shreyasrajiv327@gmail.com>
2024-07-20 13:13:10 +0530

    added output redirection

--- new_process.c
+++ new_process.c
@@ -1,45 +1,66 @@
 #include "shell.h"
+
 /**
  * new_process - create a new process
  * @args: array of strings that contains the command and its flags
  *
  * Return: 1 if success, 0 otherwise.
  */
-
 int new_process(char **args)
 {
-    pid_t pid;
+	pid_t pid;
 	int status;
+	int i;
+	char *output_file = NULL;
 
-    pid = fork();
-    if(pid == 0)
-    {
-        if(execvp(args[0],args)== -1)
-        {
-            perror("error in new process: child process");
-        }
-        exit(EXIT_FAILURE);
-    }
-    else if(pid<0)
-    {
-        perror("error in new_process : forking");
-        /* error forking */
-    }
-    else{
-        /*parent process*/
-
-        do{
-            waitpid(pid,&status,WUNTRACED);
-            //WUNTRACED tells waitpid() to return if a child has stopped due to receiving a SIGSTOP signal
-            //It allows the parent process to handle cases where the child process might be paused or stopped temporarily.
-        }while (!WIFEXITED(status) && !WIFSIGNALED(status));
-        //WIFEXITED(status) is a macro that checks whether the child process exited normally
-        //WIFSIGNALED(status) is another macro that checks whether the child process terminated due to a signal.
-        //It returns true if the child process was terminated by a signal (e.g., SIGKILL, SIGTERM).
-       /*The purpose of this loop is to suspend the execution of the parent process (waitpid() call) until the child process has completed its task.
-       It ensures that the parent process synchronizes with the child process and handles its termination properly.
-       By using WIFEXITED(status) and WIFSIGNALED(status) macros, the parent process can determine how the child process terminated and take appropriate actions based on that information.*/
-        
-    }
-    return 1;
+   for(i=0; args[i]!=NULL;i++)
+   {
+	 if(strcmp(args[i],">")==0)
+	 {
+        if(args[i+1] != NULL)
+		{
+           output_file = args[i+1];
+		   args[i] == NULL;
+		}
+		break;
+	 }
+   }
+	pid = fork();
+	if (pid ==  0)
+	{
+		if(output_file != NULL)
+		{
+			int fd = open(output_file,O_WRONLY| O_CREAT|O_TRUNC,0644);
+			if(fd ==-1)
+			{
+              perror("open");
+			  exit(EXIT_FAILURE);
+			}
+			if(dup2(fd,STDERR_FILENO)==-1)
+			{
+				perror(dup2);
+				exit(EXIT_FAILURE);
+			}
+			close(fd);
+		}
+		/* child process */
+		if (execvp(args[0], args) == -1)
+		{
+			perror("error in new_process: child process");
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		/* error forking */
+		perror("error in new_process: forking");
+	}
+	else
+	{
+		/* parent process */
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (-1);
 }
